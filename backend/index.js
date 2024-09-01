08a15433971d896ff7de196a91136c9cb05da0f3
Akaah <aakashdotgupta@gmail.com>
2024-09-01 16:20:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,38 @@ app.use("/", getAmount);
 app.use("/", minesClick);
 app.use("/", play);
 app.use("/", sendData);
-app.use("/", signin);
+// app.use("/", signin);
+app.post("/signin", async function(req,res){
+  const data = req.body;
+
+  const parsedData = schema2.safeParse(data);
+  if(!parsedData.success){
+    res.json(parsedData.error);
+    return;
+  }
+
+  const users = await user.findOne({
+    phoneNumber: data.phoneNumber
+  });
+  if(!users){
+    res.json("wrong phone number")
+    return;
+  }
+  if(bcrypt.compareSync(data.password, users.password)){
+    const session = req.session;
+    session.authen=true;
+    session.UserId=users._id;
+    session.save();
+    res.status(200).send({
+      msg:"signed in",
+      balance: users.money
+    })
+  }
+  else{
+    res.json("wrong user");
+  }
+
+});
 app.use("/", signOut);
 app.use("/", signup);
 app.use("/", updateBooks);
