a296a305b431575a25f75c58d3d9f77bebbe17e9
Akaah <aakashdotgupta@gmail.com>
2024-09-01 12:03:45 +0530

    .

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+require('dotenv').config()
+const cors = require('cors');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const bodyParser = require('body-parser');
+
+
+const getAmount = require("./routes/getAmount");
+const minesClick = require("./routes/minesClick");
+const play = require("./routes/play");
+const sendData = require("./routes/sendData");
+const signin = require("./routes/signin");
+const signOut = require("./routes/signOut");
+const signup = require("./routes/signup");
+const updateBooks = require("./routes/updateBooks");
+const updateUser = require("./routes/updateUser");
+
+
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  databaseName: 'mines',
+  collection: 'Sessions'
+});
+
+store.on('error', function(error) {
+  console.log(error);
+});
+
+
+const app = express();
+app.use(cors({
+  origin: ['http://localhost:5173', "https://khello-sata-mw8j7wla2-aka5h14s-projects.vercel.app/"],
+  credentials: true
+}
+));
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false 
+}));
+
+app.use(session({
+  secret: 'mines', 
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+    maxAge: 60000 * 60
+  },
+  store: store,
+
+}));
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.use("/", getAmount);
+app.use("/", minesClick);
+app.use("/", play);
+app.use("/", sendData);
+app.use("/", signin);
+app.use("/", signOut);
+app.use("/", signup);
+app.use("/", updateBooks);
+app.use("/", updateUser);
+
+module.exports=app;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+const PORT = process.env.PORT || 3000;
