9fafd035c263b826ea6ea6630e9729318e59ec0b
Akaah <aakashdotgupta@gmail.com>
2024-09-01 12:25:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,8 +65,9 @@ app.use("/", updateUser);
 
 module.exports=app;
 
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-const PORT = process.env.PORT || 3000;
