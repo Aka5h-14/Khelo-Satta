713b7eaa8cd957386e1b8474179498d98e2023bc
Akaah <aakashdotgupta@gmail.com>
2025-05-25 15:31:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -39,7 +39,8 @@ function App() {
   const [alertBoxSeverity, setAlertBoxSeverity] = useState("warning");
 
   // const API = "https://khelo-satta.vercel.app/api/";
-  const API = "https://khelo.100xdev.me/api/";
+  // const API = "https://khelo.100xdev.me/api/";
+  const API = "https://khelo-satta-backend.onrender.com/api/";
   // const API = "https://localhost:3000/api/";
 
 
