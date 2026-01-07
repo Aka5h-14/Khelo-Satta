e5284c3739f4675d89bac490b43f8d5f6481bb60
Akash Gupta <121353237+Aka5h-14@users.noreply.github.com>
2026-01-07 19:17:39 +0530

    changed backend url domain

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://api.khelo.100xdev.me' 
+  ? 'https://api.khelo.100xdev.tech' 
   : 'https://localhost:3000';
 
 const api = axios.create({
